f67c90c9459ae618d3998f04d1d8dd7e6750c287
Hoshiiro <91195900+yurtan2@users.noreply.github.com>
2023-12-13 22:06:13 +0700

    update App game
    id dan divnya sudah dibuat harusnya tinggal benerin classnamenya

--- WebGame_FrontEnd/src/App.tsx
+++ WebGame_FrontEnd/src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react'
 import {loadGame} from './loadGame';
+import { grey } from '@mui/material/colors';
 
 // INI CUMAN BUAT NGETES AJA, APUS & GANTI2 AJA KALO MAU -Nichoasl, 18 Nov 23
 function App() {
@@ -19,26 +20,49 @@ function App() {
       <div className="top">NavBar or something </div>
     } */}
       <div className="left-side" style={{width:"80%"}}>
+        <div style={{fontSize:"30px"}}>Game Screen</div>
         <canvas id="view">
           
         </canvas>
       </div>
       <div className="right-side"style={{width:"20%"}}>
-
         <div className="UI">
+        <div className="buttonsection">
+                <button id="save" className="button save" style={{width:"30px",height:"30px" ,color:"black"}}>
+                    Save
+                </button>
+                <button id="exit" className="button exit"  style={{width:"30px",height:"30px" ,color:"black"}}>
+                    Exit
+                </button>
+            </div>
+            <div className="Music">
+              ini harusnya musik
+            </div>
             <textarea name="console" style={{fontFamily:'monospace'}} className="console" id="console" cols={30} rows={10} spellCheck="false">
 
             </textarea>
             <div className="buttonsection">
-                <button id="executeButton" className="button start">
+                <button id="executeButton" className="button start" style={{width:"30px",height:"30px" ,color:"black"}}>
                     Start
                 </button>
-                <button id="stopButton" className="button stop">
+                <button id="stopButton" className="button stop"  style={{width:"30px",height:"30px" ,color:"black"}}>
                     Stop
+                </button>
+            </div>
+            <div id="log" style={{color:"black", width:"200px", height:"200px"}}>
+
+            </div>
+            <div className="buttonsection">
+                <button id="report" className="button report" style={{width:"30px",height:"30px" ,color:"black"}}>
+                    Report Bug
+                </button>
+                <button id="help" className="button help"  style={{width:"30px",height:"30px" ,color:"black"}}>
+                    Help
                 </button>
             </div>
         </div>
       </div>
+
     </div>
   )
 }
