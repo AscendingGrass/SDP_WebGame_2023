bf8e375052e362bbd0194aa305763100503e76f1
rubenyw <rubenyasonwinarta@gmail.com>
2023-12-14 09:17:08 +0700

    Update App.tsx

--- WebGame_FrontEnd/src/App.tsx
+++ WebGame_FrontEnd/src/App.tsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from 'react'
 import {loadGame} from './loadGame';
 import { grey } from '@mui/material/colors';
 import { Button } from '@material-tailwind/react';
+import { useNavigate } from 'react-router';
 
 // INI CUMAN BUAT NGETES AJA, APUS & GANTI2 AJA KALO MAU -Nichoasl, 18 Nov 23
 function App() {
-  const [mode, setMode] = useState(false)
+  const [mode, setMode] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
     loadGame()
   }, [])
@@ -27,36 +29,36 @@ function App() {
       </div>
       <div className="right-side"style={{width:"20%", height:"100%"}}>
         <div className="UI flex flex-col h-full">
-          <div className="buttonsection">
-            <Button color="blue" id="save"  className="button save ">
+          <div className="flex gap-4 justify-end">
+            <Button color="blue" id="save"  className="button save w-1/2">
                 Save
             </Button>
-            <Button color="red" id="exit" className="button exit" >
+            <Button color="red" id="exit" className="button exit w-1/2" onClick={()=>{navigate("/login")}} >
                 Exit
             </Button>
           </div>
           <div className="Music">
-            ini harusnya musik
+            
           </div>
-          <textarea name="console" style={{fontFamily:'monospace', width:"100%"}} className="console" id="console" cols={30} rows={22} spellCheck="false">
+          <textarea name="console" style={{fontFamily:'monospace', width:"100%"}} className="console h-2/6" id="console" cols={30} spellCheck="false">
 
           </textarea>
-          <div className="buttonsection">
-              <Button color="green" id="executeButton" className="button start" >
+          <div className="buttonsection flex gap-4 justify-end">
+              <Button color="green" id="executeButton" className="button start w-1/2" >
                   Start
               </Button>
-              <Button color="red" id="stopButton" className="button stop" >
+              <Button color="red" id="stopButton" className="button stop w-1/2" >
                   Stop
               </Button>
           </div>
           <div id="log" className="flex-grow basis-1" style={{color:"black", overflow:"scroll", padding:"5px"}}>
 
           </div>
-          <div className="buttonsection">
-              <Button id="report" className="button report" >
+          <div className="buttonsection flex gap-4">
+              <Button id="report" className="button report w-1/2" >
                   Report Bug
               </Button>
-              <Button id="help" className="button help" >
+              <Button id="help" className="button help w-1/2" >
                   Help
               </Button>
           </div>
