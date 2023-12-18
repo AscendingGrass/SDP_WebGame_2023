855bd431def215dd7967f764f262d076551a0eb5
rubenyw <104652395+rubenyw@users.noreply.github.com>
2023-12-18 18:24:32 +0700

    update back end

--- WebGame_BackEnd/src/controllers/user.js
+++ WebGame_BackEnd/src/controllers/user.js
@@ -1,8 +1,185 @@
+const { ObjectId } = require('mongodb');
 const Scoreboard = require("../models/Scoreboards");
 const User = require("../models/Users");
 const jwt = require('jsonwebtoken');
 
 const secret_key = "t12345!";
+
+const insertDummy = async (req, res) => {
+    const result = [
+        await User.insertMany(
+            [
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5b6"),
+                    "username": "adminITLAND",
+                    "password": "ITLAND123!",
+                    "email": "admin@example.com",
+                    "role": "admin",
+                    "gender": "Male",
+                    "status": "active",
+                    "created_at": new Date(1702811456858)
+                },
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5b7"),
+                    "username": "johnson",
+                    "password": "123456789",
+                    "email": "jonhson@gmail.com",
+                    "role": "user",
+                    "gender": "male",
+                    "volume": 42,
+                    "scoreboard": new ObjectId("657ed9c4dc599aa08645f5bd"),
+                    "status": "active",
+                    "created_at": new Date(1702811456858),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5b8"),
+                    "username": "mary_smith",
+                    "password": "mary123",
+                    "email": "mary.smith@email.com",
+                    "role": "user",
+                    "gender": "female",
+                    "volume": 30,
+                    "scoreboard": new ObjectId("657ed9c4dc599aa08645f5be"),
+                    "status": "active",
+                    "created_at": new Date(1702811456858),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5b9"),
+                    "username": "robert_jones",
+                    "password": "robert456",
+                    "email": "robert.jones@example.net",
+                    "role": "user",
+                    "gender": "male",
+                    "volume": 18,
+                    "scoreboard": new ObjectId("657ed9c4dc599aa08645f5bf"),
+                    "status": "active",
+                    "created_at": new Date(1702811456858),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5ba"),
+                    "username": "alice_doe",
+                    "password": "alice789",
+                    "email": "alice.doe@email.com",
+                    "role": "user",
+                    "gender": "female",
+                    "volume": 25,
+                    "scoreboard": new ObjectId("657ed9c4dc599aa08645f5c0"),
+                    "status": "active",
+                    "created_at": new Date(1702811456858),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5bb"),
+                    "username": "peter_green",
+                    "password": "green123",
+                    "email": "peter.green@example.org",
+                    "role": "user",
+                    "gender": "male",
+                    "volume": 15,
+                    "scoreboard": new ObjectId("657ed9c4dc599aa08645f5c1"),
+                    "status": "active",
+                    "created_at": new Date(1702811456858),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed740dc599aa08645f5bc"),
+                    "username": "susan_wilson",
+                    "password": "susan456",
+                    "email": "susan.wilson@email.com",
+                    "role": "user",
+                    "gender": "female",
+                    "volume": 35,
+                    "scoreboard": new ObjectId("657ed9c4dc599aa08645f5c2"),
+                    "status": "active",
+                    "created_at": new Date(1702811456858),
+                    "updated_at": null,
+                    "deleted_at": null
+                }
+            ]
+        ),
+        await Scoreboard.insertMany(
+            [
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5bd"),
+                    "user_id": "657ed740dc599aa08645f5b6",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5be"),
+                    "user_id": "657ed740dc599aa08645f5b7",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5bf"),
+                    "user_id": "657ed740dc599aa08645f5b8",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5c0"),
+                    "user_id": "657ed740dc599aa08645f5b9",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5c1"),
+                    "user_id": "657ed740dc599aa08645f5ba",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5c2"),
+                    "user_id": "657ed740dc599aa08645f5bb",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                },
+                {
+                    "_id": new ObjectId("657ed9c4dc599aa08645f5c3"),
+                    "user_id": "657ed740dc599aa08645f5bc",
+                    "score": 0,
+                    "status": "active",
+                    "created_at": new Date("2023-01-15T08:30:00.000Z"),
+                    "updated_at": null,
+                    "deleted_at": null
+                }
+            ]
+        )
+    ];
+
+    return res.status(200).json({
+        error: false,
+        msg: "Success",
+        result
+    })
+}
 
 const user = async (req, res)=> {
     const { id } = req.params;
@@ -233,6 +410,7 @@ const fetchFemale = async (req, res) => {
 
 module.exports = {
     user,
+    insertDummy,
     login,
     register,
     deleteUser,
--- WebGame_BackEnd/src/routes/user.js
+++ WebGame_BackEnd/src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express();
 
 const User = require("../models/Users");
-const { login, register, deleteUser, user, allUser, save, fetchFemale, fetchMale, allUserStatus } = require("../controllers/user");
+const { login, register, deleteUser, user, allUser, save, fetchFemale, fetchMale, allUserStatus, insertDummy } = require("../controllers/user");
 
 // GET
 router.get("/user/:id", user)
@@ -14,6 +14,7 @@ router.get("/userMale", fetchMale);
 // POST
 router.post("/login", login);
 router.post("/register", register);
+router.post("/dummyInsertUser", insertDummy);
 
 // PUT
 
