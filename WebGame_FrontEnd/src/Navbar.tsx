272f0c76a6a4db48e5233278c65e7759833c2725
rubenyw <rubenyasonwinarta@gmail.com>
2023-12-21 04:07:04 +0700

    Navbar

--- WebGame_FrontEnd/src/Navbar.tsx
+++ WebGame_FrontEnd/src/Navbar.tsx
@@ -1,7 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import {
   Navbar,
@@ -20,7 +19,7 @@ const NavBarFun = () => {
   console.log(state);
   
   const adminNavList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="flex items-center gap-6 py-2">
       <Typography
         as="li"
         variant="small"
@@ -118,11 +117,35 @@ const NavBarFun = () => {
           News
         </Link>
       </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="flex items-center gap-x-2 p-1 font-medium hover:text-blue-500 transition-colors cursor-pointer"
+      >
+        <svg
+          width="16"
+          height="15"
+          viewBox="0 0 16 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M0 2.5C0 1.96957 0.210714 1.46086 0.585786 1.08579C0.960859 0.710714 1.46957 0.5 2 0.5H14C14.5304 0.5 15.0391 0.710714 15.4142 1.08579C15.7893 1.46086 16 1.96957 16 2.5V12.5C16 13.0304 15.7893 13.5391 15.4142 13.9142C15.0391 14.2893 14.5304 14.5 14 14.5H2C1.46957 14.5 0.960859 14.2893 0.585786 13.9142C0.210714 13.5391 0 13.0304 0 12.5V2.5ZM3.293 3.793C3.48053 3.60553 3.73484 3.50021 4 3.50021C4.26516 3.50021 4.51947 3.60553 4.707 3.793L7.707 6.793C7.89447 6.98053 7.99979 7.23484 7.99979 7.5C7.99979 7.76516 7.89447 8.01947 7.707 8.207L4.707 11.207C4.5184 11.3892 4.2658 11.49 4.0036 11.4877C3.7414 11.4854 3.49059 11.3802 3.30518 11.1948C3.11977 11.0094 3.0146 10.7586 3.01233 10.4964C3.01005 10.2342 3.11084 9.9816 3.293 9.793L5.586 7.5L3.293 5.207C3.10553 5.01947 3.00021 4.76516 3.00021 4.5C3.00021 4.23484 3.10553 3.98053 3.293 3.793ZM9 9.5C8.73478 9.5 8.48043 9.60536 8.29289 9.79289C8.10536 9.98043 8 10.2348 8 10.5C8 10.7652 8.10536 11.0196 8.29289 11.2071C8.48043 11.3946 8.73478 11.5 9 11.5H12C12.2652 11.5 12.5196 11.3946 12.7071 11.2071C12.8946 11.0196 13 10.7652 13 10.5C13 10.2348 12.8946 9.98043 12.7071 9.79289C12.5196 9.60536 12.2652 9.5 12 9.5H9Z"
+            fill="#90A4AE"
+          />
+        </svg>
+        <Link to={"/admin/report"} className="flex items-center">
+          Bugs
+        </Link>
+      </Typography>
     </ul>
   );
 
   const noUserNavList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 ">
+    <ul className="py-2 flex gap-2 items-center gap-6 ">
       <Typography
         as="li"
         variant="small"
@@ -197,7 +220,7 @@ const NavBarFun = () => {
   )
 
   const userNavList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 ">
+    <ul className="py-2 flex gap-2 items-center gap-6">
       <Typography
         as="li"
         variant="small"
@@ -243,7 +266,7 @@ const NavBarFun = () => {
           />
         </svg>
         <Link to={"/leaderboard"} className="flex items-center">
-          Leader
+          Leaderboard
         </Link>
       </Typography>
       <Typography
@@ -297,7 +320,7 @@ const NavBarFun = () => {
   )
   
   return (
-    <Navbar className="mx-auto max-w-100 px-4 py-2 lg:px-8 lg:py-4 bg-black rounded-none">
+    <Navbar className="mx-auto max-w-100 px-4 py-2 items-center lg:px-8 lg:py-4 bg-black">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         {
           !state.user &&
@@ -306,8 +329,8 @@ const NavBarFun = () => {
         {
           state.user &&
           <div className="flex">
-            <Typography as="a" href="#" className="mr-0 cursor-pointer py-1.5 font-medium">Hello, &nbsp; </Typography>
-            <Typography as="a" href="#" className={`mr-4 cursor-pointer py-1.5 font-medium ${state.user.role == "admin"? "text-red-500" : "text-blue-500"}`}>
+            <Typography as="a" className="mr-0 cursor-pointer font-medium">Hello, &nbsp; </Typography>
+            <Typography as="a" className={`mr-4 cursor-pointe font-medium ${state.user.role == "admin"? "text-red-500" : "text-blue-500"}`}>
               {state.user.username}
             </Typography>
           </div>
