50bbc5a758fae43f05a3665f726d5fa5dfdd4967
rubenyw <rubenyasonwinarta@gmail.com>
2024-01-02 18:40:04 +0700

    Update Login.tsx

--- WebGame_FrontEnd/src/Login.tsx
+++ WebGame_FrontEnd/src/Login.tsx
@@ -6,22 +6,6 @@ const Login = () => {
     <div className="grid w-full h-full place-items-center">
       <LoginForm/>
     </div>
-    // <div className="grid gap-5 place-items-center ">
-    //   <div className="w-72">
-    //     <Input label="Username" name="username" onChange={handleInputChange} crossOrigin={undefined} />
-    //   </div>
-    //   <div className="w-72">
-    //     <Input label="Password" name="password" onChange={handleInputChange} crossOrigin={undefined}/>
-    //   </div>
-    //   <div className="w-72">
-    //     <Button variant="filled" color="blue"
-    //       onClick={handleLogin}
-    //     >Login</Button>
-    //   </div>
-    //   <p>
-    //     Don't have an account? <Link className="text-blue-700" to={"register"}>Sign Up Now</Link>
-    //   </p>
-    // </div>
   );
 };
 
